86b1d7d75639aaefd8b2b72898dce6696ae815a4
Twan Koolen <tkoolen@csail.mit.edu>
2015-05-08 18:20:22 -0400

    Made LockFreeMessageMonitor capable of handling multiple channels.

--- test/drake/util/LockFreeMessageMonitorTest.java
+++ test/drake/util/LockFreeMessageMonitorTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -16,36 +19,44 @@ public class LockFreeMessageMonitorTest {
 
   private static class LockFreeMessageReceiverTestTask implements Runnable {
     private final LockFreeMessageMonitor monitor = new LockFreeMessageMonitor();
-    private long last_time_stamp = -1;
-    private double difference_millis_check;
+    private final Map<String, Long> last_timestamps = new HashMap<String, Long>();
     private int messages_received = 0;
+    private final Map<String, Double> differences_millis_check;
 
-    public LockFreeMessageReceiverTestTask(String channel, double difference_millis_check) throws IOException {
-      LCM.getSingleton().subscribe(channel, monitor);
-      this.difference_millis_check = difference_millis_check;
+    public LockFreeMessageReceiverTestTask(Iterable<String> channels, Map<String, Double> differences_millis_check) throws IOException {
+      for (String channel : channels) {
+        LCM.getSingleton().subscribe(channel, monitor);
+        last_timestamps.put(channel, -1l);
+      }
+      this.differences_millis_check = differences_millis_check;
     }
 
     @Override
     public void run() {
-      byte[] byte_array = monitor.getMessage();
-      if (byte_array != null) {
-        drake.lcmt_body_wrench_data msg;
-        try {
-          msg = new drake.lcmt_body_wrench_data(byte_array);
-          if (last_time_stamp > 0) {
-            long difference_nanos = msg.timestamp - last_time_stamp;
-            double difference_millis = difference_nanos / 1e6;
-            // System.out.println("difference: " + difference_millis + " ms");
-            assertTrue(msg.timestamp > last_time_stamp);
-            if (!Double.isNaN(difference_millis_check) && messages_received > 0) {
-              double tol_millis = 10;
-              assertEquals(difference_millis, difference_millis_check, tol_millis);
+      Map<String, byte[]> byte_arrays = monitor.getMessages();
+      for (String channel : byte_arrays.keySet()) {
+        byte[] byte_array = byte_arrays.get(channel);
+        if (byte_array != null) {
+          drake.lcmt_body_wrench_data msg;
+          try {
+            msg = new drake.lcmt_body_wrench_data(byte_array);
+            long last_timestamp = last_timestamps.get(channel);
+            if (last_timestamp > 0) {
+              long difference_nanos = msg.timestamp - last_timestamp;
+              double difference_millis = difference_nanos / 1e6;
+              // System.out.println("difference: " + difference_millis + " ms");
+              assertTrue(msg.timestamp > last_timestamp);
+              double difference_millis_check = differences_millis_check.get(channel);
+              if (!Double.isNaN(difference_millis_check) && messages_received > 0) {
+                double tol_millis = 10;
+                assertEquals(difference_millis, difference_millis_check, tol_millis);
+              }
             }
+            last_timestamp = msg.timestamp;
+            messages_received++;
+          } catch (IOException e) {
+            e.printStackTrace();
           }
-          last_time_stamp = msg.timestamp;
-          messages_received++;
-        } catch (IOException e) {
-          e.printStackTrace();
         }
       }
     }
@@ -57,27 +68,46 @@ public class LockFreeMessageMonitorTest {
 
   @Test
   public void testPollFasterThanPublish() throws InterruptedException, IOException {
-    long publish_period_millis = 100;
+    Map<String, Long> publish_periods_millis = new HashMap<String, Long>();
+    int num_channels = 5;
+    for (int i = 0; i < num_channels; i++) {
+      String channel = "TEST_CHANNEL_" + i;
+      publish_periods_millis.put(channel, 100 + (i + 1) * 50l);
+    }
+    
     long poll_period_millis = 1;
     long test_duration_millis = 5 * 1000;
-    runTest(publish_period_millis, poll_period_millis, test_duration_millis);
+    runTest(publish_periods_millis, poll_period_millis, test_duration_millis);
   }
 
   @Test
   public void testPublishFasterThanPoll() throws InterruptedException, IOException {
-    long publish_period_millis = 1;
+    Map<String, Long> publish_periods_millis = new HashMap<String, Long>();
+    int num_channels = 5;
+    for (int i = 0; i < num_channels; i++) {
+      String channel = "TEST_CHANNEL_" + i;
+      publish_periods_millis.put(channel, (i + 1) * 1l);
+    }
     long poll_period_millis = 100;
     long test_duration_millis = 5 * 1000;
-    runTest(publish_period_millis, poll_period_millis, test_duration_millis);
+    runTest(publish_periods_millis, poll_period_millis, test_duration_millis);
   }
 
-  private void runTest(long publish_period_millis, long poll_period_millis, long test_duration_millis) throws IOException, InterruptedException {
-    ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-    String channel = "TEST_CHANNEL";
-    scheduler.scheduleAtFixedRate(new MessagePublisherTestTask(channel), 0, publish_period_millis, TimeUnit.MILLISECONDS);
+  private void runTest(Map<String, Long> publish_periods_millis, long poll_period_millis, long test_duration_millis) throws IOException, InterruptedException {
+    Set<String> channels = publish_periods_millis.keySet();
+
+
+    ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(channels.size());
+    for (String channel : channels) {
+      scheduler.scheduleAtFixedRate(new MessagePublisherTestTask(channel), 0, publish_periods_millis.get(channel), TimeUnit.MILLISECONDS);
+    }
+
     long num_iterations = test_duration_millis / poll_period_millis;
-    double difference_millis_check = poll_period_millis > publish_period_millis ? poll_period_millis : Double.NaN;
-    LockFreeMessageReceiverTestTask receiver_task = new LockFreeMessageReceiverTestTask(channel, difference_millis_check);
+    Map<String, Double> differences_millis_check = new HashMap<String, Double>();
+    for (String channel : channels) {
+      differences_millis_check.put(channel, poll_period_millis > publish_periods_millis.get(channel) ? poll_period_millis : Double.NaN);
+    }
+    LockFreeMessageReceiverTestTask receiver_task = new LockFreeMessageReceiverTestTask(channels, differences_millis_check);
     for (int i = 0; i < num_iterations; i++) {
       receiver_task.run();
       Thread.sleep(poll_period_millis);
